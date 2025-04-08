d6e041b2796098e95546a0da848ea13f025f4d10
FirTreeMan <61369993+FirTreeMan@users.noreply.github.com>
2025-04-07 21:53:08 -0500

    selector now finds contest data at runtime

--- src/swing/SelectorFrame.java
+++ src/swing/SelectorFrame.java
@@ -3,28 +3,31 @@ package swing;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Map;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiConsumer;
 
 public class SelectorFrame extends JFrame implements ActionListener {
     public static final String[] levels = {"District", "Region", "State"};
-    public static final Integer[] districtYears = {2024};
-    public static final Integer[] regionYears = {2024};
-    public static final Integer[] stateYears = {2024};
-    public static final Integer[][] years = {districtYears, regionYears, stateYears};
+    public static final ArrayList<Integer> districtYears = new ArrayList<>(), regionYears = new ArrayList<>(), stateYears = new ArrayList<>();
+    public static final ArrayList[] years = {districtYears, regionYears, stateYears};
 
     private final JComboBox<String> levelList = new JComboBox<>(levels);
     private final JComboBox<Integer> yearList = new JComboBox<>();
-    private final DefaultComboBoxModel[] yearModels = {
-            new DefaultComboBoxModel<>(years[0]),
-            new DefaultComboBoxModel<>(years[1]),
-            new DefaultComboBoxModel<>(years[2])
-    };
+    private final DefaultComboBoxModel[] yearModels;
 
     public SelectorFrame(BiConsumer<String, Integer> contestMaker) {
         super("UIL Computer Science Tester");
         setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        makeLevelYears();
+        yearModels = new DefaultComboBoxModel[]{
+                new DefaultComboBoxModel<>(years[0].toArray(Integer[]::new)),
+                new DefaultComboBoxModel<>(years[1].toArray(Integer[]::new)),
+                new DefaultComboBoxModel<>(years[2].toArray(Integer[]::new))
+        };
 
         levelList.addActionListener(this);
         setYears(0);
@@ -58,6 +61,23 @@ public class SelectorFrame extends JFrame implements ActionListener {
 
     private void setYears(int x) {
         yearList.setModel(yearModels[x]);
+    }
+
+    private void makeLevelYears() {
+        String pathBase = "data/";
+        districtYears.addAll(findYearsForLevel(new File(pathBase + "district").listFiles()));
+        regionYears.addAll(findYearsForLevel(new File(pathBase + "region").listFiles()));
+        stateYears.addAll(findYearsForLevel(new File(pathBase + "state").listFiles()));
+    }
+
+    private List<Integer> findYearsForLevel(File[] files) {
+        ArrayList<Integer> yearsFound = new ArrayList<>(files.length);
+        for (File file: files) {
+            // file should be a directory with contest year as name
+            if (file.isDirectory())
+                yearsFound.add(Integer.parseInt(file.getName()));
+        }
+        return yearsFound;
     }
 
     @Override
